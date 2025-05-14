f2f63fe64a7e09203ebf4b92ae283e4c3062c977
kalymosa <kalymosarcarne@gmail.com>
2025-05-13 21:05:45 -0700

    Update NixOS config: 2025-05-13 21:05:45

--- packages.nix
+++ packages.nix
@@ -21,6 +21,6 @@
  programs.steam.enable = true;
  programs.firefox.enable = true;
  services.xserver.enable = true;
- services.displayManager.sddm.enable = true;
- services.desktopManager.plasma6.enable = true;
+ services.xserver.desktopManager.budgie.enable = true;
+ services.xserver.displayManager.lightdm.enable = true;
 }
