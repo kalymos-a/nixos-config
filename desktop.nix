89cf69382d43886db77157b4046d2c7994cfab80
kalymosa <kalymosarcarne@gmail.com>
2025-05-30 01:03:44 -0700

    Update NixOS config: 2025-05-30 01:03:44

--- desktop.nix
+++ desktop.nix
@@ -1,6 +1,5 @@
 { config, pkgs, ... }:
 {
-    services.xserver.enable = true; # optional
     services.displayManager.sddm.enable = true;
     services.displayManager.sddm.wayland.enable = true;
     services.desktopManager.plasma6.enable = true;
